23fecb93440f31a7dbc94adf111a04fa73266fef
grahamna <nate.a.graham@gmail.com>
2026-02-09 14:57:53 -0800

    Made the default music player vlc, as most linux distros come with it

--- musicPlayerExe.sh
+++ musicPlayerExe.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 
     ## ---Settings---
 
-MUSIC_PLAYER="audacious"  # Name of music application, change for your specific music player. Double check by `pactl list sink-inputs | grep "application.id"` while your is active.
+MUSIC_PLAYER="vlc"  # Name of music application, change for your specific music player. Double check by `pactl list sink-inputs | grep "application.icon_name"` while your is active, or through program processes. This should be the "executable" available through the terminal
 
 PATH_TO_MUSIC_VOL_MANAGER="/PATH/TO/musicVolManager.sh" # This should point to musicVolManager.sh
 
@@ -22,10 +22,16 @@ if ! command -v $MUSIC_PLAYER &> /dev/null; then
     exit 1
 fi
 
+# Check Music Input for bad characters via Whitelist
+if [[ ! "$MUSIC_PLAYER" =~ ^[a-zA-Z0-9_-]+$ ]]; then
+    echo "Error: Invalid music player name. Exiting."
+    exit 1
+fi
+
 # Launch music player in the background.
 $MUSIC_PLAYER &
-AUD_PID=$!
+
 
 # Start the monitoring script in the background. Sleep allows for the music player to actually start before volume manager.
-sleep 0.25
+sleep 0.35
 "$PATH_TO_MUSIC_VOL_MANAGER" $MUSIC_PLAYER
